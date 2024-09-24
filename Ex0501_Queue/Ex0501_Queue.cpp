5bbdf1d4744bb1f3a8536b5c630b1fe8abdfc9da
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-09-24 19:13:41 +0300

    Add files via upload

---
+++ Ex0501_Queue/Ex0501_Queue.cpp
@@ -0,0 +1,41 @@
+﻿#include "../shared/Queue.h"
+
+#include <iostream>
+
+int main()
+{
+	Queue<char> q(2); 
+
+	q.SetDebugFlag(true);
+
+	q.Print();
+
+	q.Enqueue('A');
+	q.Print();
+
+	q.Enqueue('B');
+	q.Print();
+
+	q.Enqueue('C');
+	q.Print();
+
+	q.Enqueue('D');
+	q.Print();
+
+	q.Dequeue();
+	q.Print();
+
+	q.Dequeue();
+	q.Print();
+
+	q.Dequeue();
+	q.Print();
+
+	for (char c = 'E'; c <= 'K'; c++)
+	{
+		q.Enqueue(c);
+		q.Print();
+	}
+
+	return 0;
+}

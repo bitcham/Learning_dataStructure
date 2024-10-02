8dba6566b9c3790c942bcd7d7d2c5a52507e0051
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-02 15:39:33 +0300

    Add files via upload

---
+++ Ex0702_ExpressionTree/Ex0702_ExpressionTree.cpp
@@ -0,0 +1,289 @@
+﻿#include <iostream>
+
+#include "../shared/Stack.h"
+#include "../shared/Queue.h"
+#include "../shared/BinaryTree.h"
+
+using namespace std;
+
+// Function to return precedence of operators
+int Prec(char c);
+void InfixToPostfix(Queue<char>& q, Queue<char>& output);
+int EvalPostfix(Queue<char>& q);
+
+class ExpressionTree : public BinaryTree<char>
+{
+public:
+	typedef BinaryTree<char> Base;
+
+	ExpressionTree(Node* root) : Base(root) {}
+
+	bool IsDigit(char c)
+	{
+		if (c >= '0' && c <= '9') return true;
+		else return false;
+	}
+
+	int Evaluate()
+	{
+		return Evaluate(Base::root_);
+	}
+
+	int Evaluate(Node* node)
+	{
+		if (!node) return 0;
+
+		if (IsDigit(node->item))
+			return node->item - '0';
+		else if (node->item == '*')
+			return Evaluate(node->left) * Evaluate(node->right);
+		else if (node->item == '+')
+			return Evaluate(node->left) + Evaluate(node->right);
+		else if (node->item == '-')
+			return Evaluate(node->left) - Evaluate(node->right);
+		else
+			return Evaluate(node->left) / Evaluate(node->right);
+	}
+
+	void Infix() { Infix(root_); cout << endl; }
+	void Infix(Node* node)
+	{
+		if (node)
+		{
+			if (!IsDigit(node->item)) cout << "(";
+			Infix(node->left);
+			cout << node->item;
+			Infix(node->right);
+			if (!IsDigit(node->item)) cout << ")";
+		}
+	}
+
+
+	void Postfix() { Postfix(root_);  cout << endl; }
+	void Postfix(Node* node) {
+		if (node)
+		{
+			Postfix(node->left);
+			Postfix(node->right);
+			cout << node->item;
+		}
+	}
+
+	// Infix -> postfix -> expression tree
+	ExpressionTree(const char* infix)
+	{
+		// Infix -> Postfix
+		Queue<char> q;
+		for (int i = 0; infix[i] != '\0'; i++)
+			q.Enqueue(infix[i]);
+		cout << "  Infix: ";
+		q.Print();
+		Queue<char> postfix;
+		InfixToPostfix(q, postfix);
+		cout << "Postfix: ";
+		postfix.Print();
+
+		// Postfix -> Expression tree
+
+		Stack<Node*> s;
+
+		while (!postfix.IsEmpty())
+		{
+			char c = postfix.Front();
+			postfix.Dequeue();
+
+			if (c >= '0' && c <= '9')
+			{
+				Node* temp = new Node{ c, nullptr, nullptr };
+				s.Push(temp);
+			}
+			else
+			{
+				Node* temp = new Node{ c, nullptr, nullptr };
+				temp->right = s.Top();
+				s.Pop();
+				temp->left = s.Top();
+				s.Pop();
+				s.Push(temp);
+			}
+		}
+
+		root_ = s.Top();
+	}
+};
+
+int main()
+{
+	using Node = ExpressionTree::Node;
+
+	// 5 + (3 - 2) * 4
+	// 
+	//    +
+	//	/   \
+	// 5     *
+	//	    / \
+	//	   -   4
+	//	  / \
+	//	 3   2
+
+	Node* n1 = new Node{ '5', nullptr, nullptr };
+	Node* n2 = new Node{ '+', nullptr, nullptr };
+	Node* n3 = new Node{ '3', nullptr, nullptr };
+	Node* n4 = new Node{ '-', nullptr, nullptr };
+	Node* n5 = new Node{ '2', nullptr, nullptr };
+	Node* n6 = new Node{ '*', nullptr, nullptr };
+	Node* n7 = new Node{ '4', nullptr, nullptr };
+
+	// +
+	n2->left = n1; // 5
+	n2->right = n6; // *
+
+	// *
+	n6->left = n4; // -
+	n6->right = n7; // 4
+
+	// -
+	n4->left = n3; // 3
+	n4->right = n5; // 2
+
+	ExpressionTree tree(n2);
+
+	tree.Print2D();
+
+	cout << "Evaluated = " << tree.Evaluate() << endl; // Evaluated = 9
+
+	cout << "  Infix: ";
+	tree.Infix();   // (5+((3-2)*4)) 
+
+	cout << "Postfix: ";
+	tree.Postfix(); // 532-4*+
+
+	cout << endl;
+
+	// Infix -> Postfix -> Expression Tree
+	{
+		// const char infix[] = "1+(1*2+3)*4";
+		const char infix[] = "(5+((3-2)*4))";
+
+		ExpressionTree tree(infix);
+
+		tree.Print2D();
+
+		cout << "Evaluated = " << tree.Evaluate() << endl; // Evaluated = 9
+	}
+
+	return 0;
+}
+
+// Function to return precedence of operators
+int Prec(char c)
+{
+	if (c == '/' || c == '*')
+		return 2;
+	else if (c == '+' || c == '-')
+		return 1;
+	else
+		return -1;
+}
+
+void InfixToPostfix(Queue<char>& q, Queue<char>& output)
+{
+	Stack<char> s; 
+
+	output.SetDebugFlag(false);
+
+	while (!q.IsEmpty())
+	{
+		char c = q.Front();
+		q.Dequeue();
+
+		//cout << c << endl;
+
+		if (c >= '0' && c <= '9') 
+			output.Enqueue(c);
+		else if (c == '(') 
+			s.Push(c);
+		else if (c == ')')
+		{
+			while (s.Top() != '(')
+			{
+				output.Enqueue(s.Top());
+				s.Pop();
+			}
+			s.Pop(); 
+		}
+		else 
+		{
+			while (!s.IsEmpty() && Prec(c) <= Prec(s.Top()))
+			{
+				output.Enqueue(s.Top());
+				s.Pop();
+			}
+
+			s.Push(c);
+		}
+
+		//cout << "Stack: ";
+		//s.Print();
+		//cout << "Output:";
+		//output.Print();
+		//cout << endl;
+	}
+
+	while (!s.IsEmpty())
+	{
+		output.Enqueue(s.Top());
+		s.Pop();
+	}
+}
+
+int EvalPostfix(Queue<char>& q)
+{
+	Stack<int> s;
+
+	while (!q.IsEmpty())
+	{
+		char c = q.Front();
+		q.Dequeue();
+
+		//cout << c << endl;
+
+		if (c != '+' && c != '-' && c != '*' && c != '/')
+		{
+			s.Push(c - '0'); 
+		}
+		else
+		{
+			//cout << "Operator: " << c << endl;
+
+			int op2 = s.Top();
+			s.Pop();
+			int op1 = s.Top();
+			s.Pop();
+
+			if (c == '+') {
+				s.Push(op1 + op2);
+			}
+			else if (c == '-') {
+				s.Push(op1 - op2);
+			}
+			else if (c == '*') {
+				s.Push(op1 * op2);
+			}
+			else if (c == '/')
+			{
+				s.Push(op1 / op2);
+			}
+			else
+			{
+				cout << "Wrong operator" << endl;
+				exit(-1); 
+			}
+		}
+
+		//cout << "Stack: ";
+		//s.Print();
+	}
+
+	return s.Top();
+}
---
+++ Ex0703_ThreadedBinaryTree/Ex0703_ThreadedBinaryTree.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class ThreadedBinaryTree
+{
+public:
+	struct Node
+	{
+		T data = T();
+		Node* left = nullptr; // Left child
+		Node* right = nullptr; // Right child
+		int right_thread = 0;
+	};
+
+	ThreadedBinaryTree(Node* root)
+	{
+		root_ = root;
+	}
+
+	Node* Next(Node* node)
+	{
+		Node* next = node->right;
+
+		if (!next || node->right_thread) return next; 
+
+		while (next->left) next = next->left;
+
+		return next;
+	}
+
+	void Inorder() { Inorder(root_); }
+	void Inorder(Node* node)
+	{
+		while (node->left) node = node->left; 
+
+		do
+		{
+			cout << node->data << " "; 
+			node = Next(node);
+		} while (node);
+
+		cout << endl;
+	}
+
+	/*void IterInorder()
+	{
+		if (!root_) return;
+
+		Stack<Node*> s;
+
+		Node* current = root_;
+		while (current || !s.IsEmpty())
+		{
+			while (current)
+			{
+				s.Push(current);
+				current = current->left;
+			}
+
+			current = s.Top();
+			s.Pop();
+
+			Visit(current);
+
+			current = current->right;
+		}
+	}*/
+
+private:
+	Node* root_ = nullptr;
+};
+
+int main()
+{
+	using Node = ThreadedBinaryTree<int>::Node;
+
+	Node* n1 = new Node{ 1, nullptr, nullptr }; 
+	Node* n2 = new Node{ 2, n1, nullptr };
+	Node* n3 = new Node{ 3, nullptr, nullptr };
+	Node* n4 = new Node{ 4, nullptr, nullptr };
+	Node* n5 = new Node{ 5, nullptr, n4 };
+	Node* n6 = new Node{ 6, n2, n5 };
+	n1->right = n3;
+
+	n3->right = n2;
+	n3->right_thread = 1;
+
+	n2->right = n6;
+	n2->right_thread = 1;
+
+	ThreadedBinaryTree<int> tree(n6);
+
+	tree.Inorder(); // 1 3 2 6 5 4
+
+	return 0;
+}
+

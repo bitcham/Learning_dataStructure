3f1df48286a3e465c071beff56cc5f5793a1b319
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-03 22:37:18 +0300

    Add files via upload

---
+++ Ex0802_PriorityQueue/Ex0802_PriorityQueue.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+
+#include "../shared/MaxHeap.h"
+
+using namespace std;
+
+struct Patient
+{
+	int severity; 
+	int time;	  
+	const char* name;
+
+	friend bool operator >= (const Patient& a, const Patient& b)
+	{
+		if (a.severity == b.severity) 
+			return a.time <= b.time;   
+		else
+			return a.severity > b.severity; 
+	}
+
+	friend bool operator > (const Patient& a, const Patient& b)
+	{
+		if (a.severity == b.severity)
+			return a.time < b.time;
+		else
+			return a.severity > b.severity;
+	}
+
+	friend bool operator < (const Patient& a, const Patient& b)
+	{
+		return !(a >= b);
+	}
+
+	friend std::ostream& operator << (std::ostream& os, const Patient& p)
+	{
+		return os;
+	}
+};
+
+int main()
+{
+	
+	MaxHeap<Patient> h;
+
+
+	h.Push({ 1, 0, "Ironman" });   
+	h.Push({ 1, 1, "Nick Fury" }); 
+	h.Push({ 3, 2, "Hulk" });     
+
+	cout << h.Top().name << endl;  
+	h.Pop();
+
+	cout << h.Top().name << endl; 
+	h.Pop();
+
+	h.Push({ 2, 3, "Blue Beetle" });
+
+	cout << h.Top().name << endl; 
+	h.Pop();
+
+	cout << h.Top().name << endl;  
+	h.Pop();
+
+
+	return 0;
+}

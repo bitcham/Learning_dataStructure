cc911fce0afb4763ba5b6416cd5577341d2c83b2
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-03 21:46:06 +0300

    Add files via upload

---
+++ Ex0801_Heap/Ex0801_Heap.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+#include "../shared/MaxHeap.h"
+
+using namespace std;
+
+int main()
+{
+	MaxHeap<int> h;
+
+	for (auto i : { 2, 8, 5, 3, 2, 1, 9, 3, 7 })
+	{
+		h.Push(i);
+		h.Print();
+	}
+
+	while (!h.IsEmpty())
+	{
+		cout << h.Top() << " ";
+		h.Pop();
+		// h.Print();
+	}
+	cout << endl;
+
+	return 0;
+}

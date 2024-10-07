efb4d3af8cb790e314057ca8b23d3a9d469417b4
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-07 21:13:36 +0300

    Add files via upload

---
+++ Ex1003_QuickSort/Ex1003_QuickSort.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+
+bool CheckSorted(int* arr, int size)
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		if (arr[i] > arr[i + 1])
+			return false;
+	}
+
+	return true;
+}
+
+void Print(int* arr, int low, int high, int n)
+{
+	for (int i = 0; i < n; i++)
+		if (i >= low && i <= high)
+			cout << setw(3) << arr[i] << " ";
+		else
+			cout << "    ";
+	cout << endl;
+}
+
+// Hoare partition scheme
+int Partition(int arr[], int low, int high, int n)
+{
+	int pivot = arr[size_t(floorf((high - low) / 2.0f)) + low];
+	int i = low - 1;
+	int j = high + 1;
+
+	while (true)
+	{
+
+		do {
+			++i;
+		} while (pivot > arr[i]);
+
+
+		do {
+			--j;
+		} while (pivot < arr[j]);
+
+		if (i >= j)
+			return j;
+
+		swap(arr[i], arr[j]);
+
+		cout << "pivot=" << pivot << ", i=" << i << ", j=" << j << endl;
+		cout << "         ";
+		Print(arr, low, high, n);
+	}
+}
+
+void QuickSort(int arr[], int low, int high, int n) 
+{
+	if (low >= 0 && high >= 0 && low < high)
+	{
+		// Partitioning Index
+		int p = Partition(arr, low, high, n);
+
+		QuickSort(arr, low, p, n); 
+		QuickSort(arr, p + 1, high, n);
+	}
+}
+
+int main()
+{
+	int arr[] = { 4, 17, 2, 9, 7, 5, 8, 1, 14, 6 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	cout << "         ";
+	Print(arr, 0, n - 1, n);
+	cout << endl;
+
+	QuickSort(arr, 0, n - 1, n);
+
+	cout << "         ";
+	Print(arr, 0, n - 1, n);
+	cout << endl;
+}
